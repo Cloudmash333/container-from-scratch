94194e366800fdad55f0c389f1391e0e12ad7233
Cloudmash <kajolgaur26@gmail.com>
2025-10-17 21:53:29 +0530

    Update attach_cgroup.sh
    Fixing path of cpu.procs

--- attach_cgroup.sh
+++ attach_cgroup.sh
@@ -7,6 +7,6 @@ echo "50000 100000" | sudo tee /sys/fs/cgroup/mycgroup/cpu.max
 echo "500000000" | sudo tee /sys/fs/cgroup/mycgroup/memory.max
 
 ps aux | grep "mynetworkns"  #Get process id of 2nd networkns process , because first is sudo process and last one is grep itself
-echo '<your process id here>' > cgroup.procs # Add process ID of network namespace process here
+echo '<your process id here>' > /sys/fs/cgroup/mycgroup/cgroup.procs # Add process ID of network namespace process here
 #verify whether cgroup changed to mycgroup for mynetworkns process
 cat /proc/<id of process>/cgroup # this should show something like 0::mycgroup
